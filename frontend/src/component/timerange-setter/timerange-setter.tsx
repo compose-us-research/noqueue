0229f3a6afa3cde8fd090cae22a12ea063639275
Joern Bernhardt <joern.bernhardt@compose.us>
2020-05-30 23:57:04 +0200

    Add validation to day selector and add error message when no date is selected
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- frontend/src/component/timerange-setter/timerange-setter.tsx
+++ frontend/src/component/timerange-setter/timerange-setter.tsx
@@ -16,23 +16,26 @@ import ErrorBoundary from "../error-boundary/error-boundary";
 import { checkTime } from "../../lib/calculate-max-duration/calculate-max-duration";
 
 interface TimerangeSetterProps {
+  index: number;
   label: string;
-  prefix: string;
+  name: string;
   range: Timerange;
   remover: () => void;
 }
 
 const TimerangeSetter: React.FC<TimerangeSetterProps> = ({
+  index,
   label,
-  prefix,
+  name,
   range,
   remover,
 }) => {
   const [isOpen, setOpen] = useState<boolean>(false);
   const [days, setDays] = useState<string>(daysToString(range.days));
-  const { watch } = useFormContext();
+  const { errors, watch } = useFormContext();
   const slots = `${days} ${range.start} - ${range.end} Uhr`;
   const toggleOpen = useCallback(() => setOpen((open) => !open), [setOpen]);
+  const prefix = `${name}[${index}]`;
   const start = watch(`${prefix}.start`, range.start);
   const end = watch(`${prefix}.end`, range.end);
   const checkTimeValidator = useCallback<Validate>((value) => {
@@ -69,6 +72,9 @@ const TimerangeSetter: React.FC<TimerangeSetterProps> = ({
           name={`${prefix}.days`}
           onChange={(days) => setDays(daysToString(days))}
         />
+        {errors.ranges && errors.ranges[index] && errors.ranges[index].days && (
+          <div>Mindestens ein Tag muss ausgewählt werden.</div>
+        )}
 
         <Spacer />
 
