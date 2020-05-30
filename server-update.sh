24935ae06fdc74b3c0ad24237dbc8fb078f7e342
Joern Bernhardt <joern.bernhardt@compose.us>
2020-05-30 04:39:58 +0200

    Use correct docker-compose config and paths on server
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- server-update.sh
+++ server-update.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
-docker-compose build -t noqueue
+source .envfile
+
+if [ ! -r ".envfile" ]; then
+  echo "Please create and use the .envfile to set all variables"
+  exit 1
+fi
+
+source .envfile
+
+if [ -z "${SERVER_IP}" ]; then
+  echo "Need to set SERVER_IP"
+  exit 1
+fi
+if [ -z "${USER_NAME}" ]; then
+  echo "Need to set USER_NAME"
+  exit 1
+fi
 
 FILENAME="platzhalterio.latest.tar"
 
@@ -14,17 +30,21 @@ echo "$(date) - Zipping file for easier transfer"
 gzip "${FILENAME}"
 
 echo "$(date) - Uploading $(du -m ${FILENAME}.gz | cut -f1) mb file to webserver"
-scp -v "${FILENAME}.gz" "${USER_NAME}@${SERVER_IP}:/home/${USER_NAME}/platzhalterio"
+scp "${FILENAME}.gz" "${USER_NAME}@${SERVER_IP}:/home/${USER_NAME}/platzhalterio"
 
 echo "$(date) - Upload complete, removing local docker image"
 rm "${FILENAME}.gz"
 
 echo "$(date) - Deploying the new version"
 
+scp "docker-compose.yml" "${USER_NAME}@${SERVER_IP}:/home/${USER_NAME}/platzhalterio"
 
 ssh -l "${USER_NAME}" "${SERVER_IP}" <<EOF
 cat <<DEPLOYMENT | bash
 set -e
+
+echo "\$(date) - Changing work directory to platzhalterio"
+cd "/home/${USER_NAME}/platzhalterio"
 
 echo "\$(date) - Unzipping uploaded docker file"
 gunzip "${FILENAME}.gz"
