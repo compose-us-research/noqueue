18464ebac3a481118aa2d252584d9e1875f39405
Joern Bernhardt <joern.bernhardt@compose.us>
2020-07-29 23:51:18 +0200

    Add migration to set slots per day
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- backend/lib/db/migrations.js
+++ backend/lib/db/migrations.js
@@ -6,5 +6,21 @@ const migrations = [
       client.query("INSERT INTO version (current_version) VALUES (1)");
     },
   },
+  {
+    version: 2,
+    update: async (client) => {
+      const result = await client.query("SELECT name FROM public.shops;");
+      await results.rows.reduce(async (a, shop) => {
+        await a;
+        const name = shop.name;
+        // whole day + availability
+        return client.query(`CREATE TABLE "${name}_dayslots" (
+          start date,
+          end date,
+          available boolean default false
+        )`);
+      }, Promise.resolve());
+    },
+  },
 ];
 module.exports = migrations;
