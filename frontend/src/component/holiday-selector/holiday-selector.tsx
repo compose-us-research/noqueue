805a547c2c5168d6c10402c66277785ce159e9c9
Joern Bernhardt <joern.bernhardt@compose.us>
2020-08-08 00:01:19 +0200

    Use value of holiday array field in range
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- frontend/src/component/holiday-selector/holiday-selector.tsx
+++ frontend/src/component/holiday-selector/holiday-selector.tsx
@@ -1,35 +1,32 @@
 import React from "react";
 import cn from "classnames";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 import { ReactComponent as PlusIcon } from "../../asset/image/plus-icon.svg";
+import { ReactComponent as RemoveIcon } from "../../asset/image/remove-icon.svg";
 import styles from "./holiday-selector.module.css";
 import { useFormContext, useFieldArray } from "react-hook-form";
 import Button from "../button/button";
 import Spacer from "../spacer/spacer";
-import Stub from "../stub/stub";
 
 interface HolidaySelectorProps {
   name: string;
 }
 
 interface RangePickerProps {
-  endDate: Date;
+  holiday: any;
   onChange: (dates: [Date, Date]) => void;
-  startDate: Date;
 }
 
-const RangePicker: React.FC<RangePickerProps> = ({
-  endDate,
-  startDate,
-  onChange,
-}) => {
+const RangePicker: React.FC<RangePickerProps> = ({ holiday, onChange }) => {
   return (
     <DatePicker
-      endDate={endDate}
+      endDate={holiday.end}
       inline
       onChange={onChange}
       selectsRange
-      startDate={startDate}
+      startDate={holiday.start}
     />
   );
 };
@@ -43,16 +40,27 @@ const HolidaySelector: React.FC<HolidaySelectorProps> = ({ name }) => {
     <div className={cn(styles.root)}>
       {fields.map((holiday, index) => {
         const id = holiday.id as any;
+        const prefix = `${name}[${index}]`;
+        console.log({ holiday });
         return (
           <React.Fragment key={id}>
             <RangePicker
-              endDate={holiday.end}
+              holiday={holiday}
               onChange={([start, end]) => {
-                setValue(`${name}[${index}].start`, start);
-                setValue(`${name}[${index}].end`, end);
+                console.log({ start, end });
+                setValue(prefix, { ...holiday, start, end });
               }}
-              startDate={holiday.start}
             />
+            <Button
+              className={styles.addButton}
+              onClick={() => {
+                remove(index);
+              }}
+              variant="secondary"
+            >
+              <span>Urlaubszeiten löschen</span>
+              <RemoveIcon />
+            </Button>
             <Spacer />
           </React.Fragment>
         );
@@ -63,6 +71,8 @@ const HolidaySelector: React.FC<HolidaySelectorProps> = ({ name }) => {
         onClick={() => {
           const appendHoliday = {
             id: ++id,
+            start: new Date(),
+            end: new Date(+new Date() + 1000 * 60 * 60 * 24),
           };
           append(appendHoliday);
         }}
