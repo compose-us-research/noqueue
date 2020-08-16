d126308b9c5a5dca9cff63d45d9f68e008b99776
Joern Bernhardt <joern.bernhardt@compose.us>
2020-08-17 01:55:29 +0200

    Try with setValue
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- frontend/src/component/range-picker/range-picker.tsx
+++ frontend/src/component/range-picker/range-picker.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import DatePicker from "react-datepicker";
 import { useFormContext, Controller } from "react-hook-form";
 import "react-datepicker/dist/react-datepicker.css";
+import { watch } from "fs";
 
 interface RangePickerProps {
   range: { id?: any; start?: Date; end?: Date };
@@ -9,16 +10,21 @@ interface RangePickerProps {
 }
 
 const RangePicker: React.FC<RangePickerProps> = ({ range, name }) => {
-  const { start, end } = range;
+  const { setValue } = useFormContext();
+  console.log("using", { name, range });
   return (
     <Controller
-      defaultValue={{ start, end }}
+      defaultValue={range}
       name={name}
       render={({ onChange, value }) => (
         <DatePicker
           endDate={value.end}
           inline
-          onChange={([start, end]: any) => onChange({ ...range, start, end })}
+          onChange={([start, end]: any) => {
+            onChange({ ...range, start, end });
+            setValue(`${name}.start`, start);
+            setValue(`${name}.end`, end);
+          }}
           selectsRange
           startDate={value.start}
         />
