10e297e1e4ac5a98570db481eb4be8176e6e112f
Joern Bernhardt <joern.bernhardt@compose.us>
2020-09-08 00:14:46 +0200

    Style radios a bit
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- frontend/src/component/radio/radio.story.tsx
+++ frontend/src/component/radio/radio.story.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Radio from "./radio";
 import Form from "../../../.storybook/helper/form";
+import Spacer from "../spacer/spacer";
 
 export default {
   title: "Radio",
@@ -10,7 +11,25 @@ export default {
 export const Default = () => (
   <Form>
     <Radio name="test" value="1" label="First" />
+    <Spacer />
     <Radio name="test" value="2" label="Second" />
+    <Spacer />
     <Radio name="test" value="3" label="Third" />
+  </Form>
+);
+
+export const MultipleRadios = () => (
+  <Form>
+    <Radio name="testA" value="1" label="A) First" />
+    <Spacer />
+    <Radio name="testA" value="2" label="A) Second" />
+    <Spacer />
+    <Radio name="testA" value="3" label="A) Third" />
+    <Spacer />
+    <Radio name="testB" value="1" label="B) First" />
+    <Spacer />
+    <Radio name="testB" value="2" label="B) Second" />
+    <Spacer />
+    <Radio name="testB" value="3" label="B) Third" />
   </Form>
 );
--- frontend/src/component/radio/radio.tsx
+++ frontend/src/component/radio/radio.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useFormContext } from "react-hook-form";
 
 import styles from "./radio.module.css";
+import Spacer from "../spacer/spacer";
 
 interface RadioProps {
   label: string;
@@ -12,12 +13,19 @@ interface RadioProps {
 const Radio: React.FC<RadioProps> = ({ label, name, value }) => {
   const { register } = useFormContext();
   return (
-    <div className={styles.root}>
-      <label>
-        <input type="radio" ref={register} name={name} value={value} />
-        {label}
-      </label>
-    </div>
+    <label className={styles.root}>
+      <Spacer direction="column" />
+      <input
+        className={styles.input}
+        type="radio"
+        ref={register}
+        name={name}
+        value={value}
+      />
+      <div className={styles.radio}></div>
+      <Spacer direction="column" />
+      <div className={styles.label}>{label}</div>
+    </label>
   );
 };
 
